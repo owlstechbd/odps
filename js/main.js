55b5968531f2ac81c90d35bc4977c3eaea247efd
owlstechbd <owlstech.net@gmail.com>
2024-09-05 20:38:15 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true, 
+    autoplayTimeout: 3000, 
+    autoplayHoverPause: true,
+    nav: false,
+    margin:30,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 1
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+
+  $('.custom-nav .prev').click(function() {
+    $('.owl-carousel').trigger('prev.owl.carousel');
+  });
+
+  $('.custom-nav .next').click(function() {
+    $('.owl-carousel').trigger('next.owl.carousel');
+  });
+});
+
+
+// animation
+
+  new WOW().init();
